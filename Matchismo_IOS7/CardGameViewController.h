67866570ffe11acd321202357be83c03a2be8248
Felipe Valduga <fvalduga@Felipe-Valdugas-MacBook-Air.local>
2013-11-04 23:26:11 -0200

    Initial Commit

---
+++ Matchismo_IOS7/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  Matchismo_IOS7
+//
+//  Created by Felipe Valduga on 11/4/13.
+//  Copyright (c) 2013 CS193P. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
---
+++ Matchismo_IOS7/CardGameViewController.m
@@ -0,0 +1,29 @@
+//
+//  CardGameViewController.m
+//  Matchismo_IOS7
+//
+//  Created by Felipe Valduga on 11/4/13.
+//  Copyright (c) 2013 CS193P. All rights reserved.
+//
+
+#import "CardGameViewController.h"
+
+@interface CardGameViewController ()
+
+@end
+
+@implementation CardGameViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
